8d05adb8675d7dabc0151752641773be77a844c9
Alexandra Voinea <stefania.alexandra.voinea@gmail.com>
2023-05-20 01:08:16 +0300

    parse response to object, show list of food in the add food section

--- app/src/main/java/com/example/fitnessfriend/services/Food.java
+++ app/src/main/java/com/example/fitnessfriend/services/Food.java
@@ -3,5 +3,6 @@ package com.example.fitnessfriend.services;
 public class Food {
     public String foodId;
     public String label;
+    public Double ENERC_KCAL;
     public Nutrient[] nutrients;
 }
