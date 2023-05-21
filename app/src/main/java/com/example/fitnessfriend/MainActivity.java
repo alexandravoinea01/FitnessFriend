e50c165555107c59f276cacb0c8565c9f686e59a
Raducanu Andrei Cosmin <75930794+andreicosmin55@users.noreply.github.com>
2023-05-21 17:13:59 +0300

    Added motivational video playback in Home tab

--- app/src/main/java/com/example/fitnessfriend/MainActivity.java
+++ app/src/main/java/com/example/fitnessfriend/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.fitnessfriend;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,8 +24,7 @@ public class MainActivity extends AppCompatActivity
     BottomNavigationView bottomNavigationView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -26,7 +34,16 @@ public class MainActivity extends AppCompatActivity
         bottomNavigationView
                 .setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.home);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("My notification", "My notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        notification();
     }
+
     HomeFragment homeFragment = new HomeFragment();
     CaloryIntakeFragment caloryIntakeFragment = new CaloryIntakeFragment();
     WorkoutFragment workoutFragment = new WorkoutFragment();
@@ -34,8 +51,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean
-    onNavigationItemSelected(@NonNull MenuItem item)
-    {
+    onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.home:
                 getSupportFragmentManager()
@@ -66,5 +82,27 @@ public class MainActivity extends AppCompatActivity
                 return true;
         }
         return false;
+    }
+
+    public void notification() {
+//        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+//        intent.putExtra("FRAGMENT", "calorieIntakeFragment");
+//        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+//
+//        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "My notification");
+        builder.setContentTitle("My title");
+        builder.setContentText("Notification! AAABBBCCCDDD"); // Corrected method name
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+        builder.setAutoCancel(true);
+        //builder.setContentIntent(pendingIntent);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        managerCompat.notify(1, builder.build());
+
     }
 }
